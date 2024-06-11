2503d4fe27f14ab9d255f372fa733fcc4e69edfe
Vidhu-sri <118075691+Vidhu-sri@users.noreply.github.com>
2024-06-11 16:43:47 +0530

    changed host

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def process_resumes():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=8000,debug=True)
+    app.run(host = 'http://0.0.0.0', port=8000,debug=True)
